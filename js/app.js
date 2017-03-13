797cd1718f03a461c5182fe63d9ba28828c0d045
Meshiest <meek.mesh@gmail.com>
2017-03-13 16:27:36 -0500

    add blank profile picture, update app.js

--- js/app.js
+++ js/app.js
@@ -9,15 +9,42 @@
     .when("/technology", {
       templateUrl: "views/technology.html"
     })
+    .when("/technology", {
+      templateUrl: "views/technology.html"
+    })
     .when("/life-sciences", {
       templateUrl: "views/life-sciences.html"
     });
   });
 
-  app.controller('AppCtrl', function($scope, $location){
+  app.controller('AppCtrl', function($scope, $location) {
     $scope.isActive = function(route) {
       return route === $location.path();
     };
+  });
+
+  app.controller('PortfolioCtrl', function($scope, $http) {
+    var portfolio = this;
+
+    // Loads the portfolio json file
+    portfolio.load = function() {
+      portfolio.loading = true;
+      portfolio.error = false;
+      portfolio.portfolio = 
+
+      $http.get("portfolio.json")
+        .then(function(resp) {
+          portfolio.loading = false;
+          portfolio.entries = resp.data;
+
+        }, function(err) {
+          console.warn("ERROR", err);
+          portfolio.loading = false;
+          portfolio.error = true;
+        });
+    };
+
+    portfolio.load();
   });
 
 })();
