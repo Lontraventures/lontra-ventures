27b6ee55447748a08b0754d33a0a733fad8e68a4
Meshiest <meek.mesh@gmail.com>
2017-06-09 13:44:59 -0500

    Added headshots, improved life-sciences, more css enhancements

--- js/app.js
+++ js/app.js
@@ -1,19 +1,24 @@
 (function(){
+  window.onscroll = function() {
+    var scroll = document.body.scrollTop;
+    $('#navbar')[0].className = scroll < 5 ? 'top' : '';
+  }
+
   var app = angular.module('app', ['ngRoute', 'slick']);
 
   app.config(function($routeProvider) {
     $routeProvider
-    .when("/", {
-      templateUrl: "views/home.html"
+    .when('/', {
+      templateUrl: 'views/home.html'
     })
-    .when("/technology", {
-      templateUrl: "views/technology.html"
+    .when('/technology', {
+      templateUrl: 'views/technology.html'
     })
-    .when("/technology", {
-      templateUrl: "views/technology.html"
+    .when('/technology', {
+      templateUrl: 'views/technology.html'
     })
-    .when("/life-sciences", {
-      templateUrl: "views/life-sciences.html"
+    .when('/life-sciences', {
+      templateUrl: 'views/life-sciences.html'
     });
   });
 
@@ -32,13 +37,13 @@
       portfolio.error = false;
       portfolio.portfolio = 
 
-      $http.get("portfolio.json")
+      $http.get('portfolio.json')
         .then(function(resp) {
           portfolio.loading = false;
           portfolio.entries = resp.data;
 
         }, function(err) {
-          console.warn("ERROR", err);
+          console.warn('ERROR', err);
           portfolio.loading = false;
           portfolio.error = true;
         });
